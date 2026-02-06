7cb75ee5818939eca56f2a4b8ffff1308ae00da2
turin <matkhan01@gmail.com>
2026-02-07 00:20:26 +0600

    basic transaction page created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from '../features/dashboard/dashboard';
+import { TransactionComponent } from '../features/transaction-component/transaction-component';
 
 export const routes: Routes = [
 	{ path: '', component: DashboardComponent },
 	{ path: 'dashboard', component: DashboardComponent },
+	{ path: 'transactions', component: TransactionComponent },
 	{ path: '**', redirectTo: '' }
 ];
